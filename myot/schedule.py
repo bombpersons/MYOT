d93039418f3a176cf5c6970700a8e287df82c780
Bombpersons <blindrabbits@gmail.com>
2009-09-30 21:54:04 +0100

    Rename schedule class to timer, and added a schedule class to manage timers.

--- myot/schedule.py
+++ myot/schedule.py
@@ -1,36 +1,19 @@
-# This is a class that schedules tasks. It will call it's ring() funtion
-# when the timer starts, and call it's running() function when within the
-# time limit, and call it's over() function when the time is up.
+from timer import Timer
 
-# This class uses SYSTEM time.
-
-class Timer:
+# This class manages the tv's schedule (if used)
+class Schedule:
 	# INIT -------------------------------------------------------------
-	# Init vars
+	# Init default vars
 	def __init__(self):
 		pass
 		
-	# SET --------------------------------------------------------------
-	# Sets the timer
-	def set(self, startTime, endTime, format):
-		pass
-	
-	# RING -------------------------------------------------------------
-	# This function is called when the timer starts.
-	def ring(self):
-		pass
-
-	# RUNNING ----------------------------------------------------------
-	# Called when the the time is whithin the time limits.
-	def running(self):
-		pass
-	
-	# OVER -------------------------------------------------------------
-	# Called when the time is up
-	def over(self):
+	# ADD --------------------------------------------------------------
+	# Add a scheduled time
+	def add(self, startTime, endTime, format, program):
 		pass
 		
-	# TICK -------------------------------------------------------------
+	# UDPDATE ----------------------------------------------------------
 	# Call this every loop (or in a seperate process)
-	def tick(self):
+	def update(self):
 		pass
+
